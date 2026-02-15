ac6ac6568053b966b1b2cf024f2538e48653a063
sasi preetham chandaka <87517348+sasipreethamchandaka@users.noreply.github.com>
2026-02-15 14:44:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,57 @@
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name = "my_vpcc"
-    }
-  
+provider "aws" {
+  region = "us-east-1"
 }
 
+# ---------------- VPC ----------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-resource "aws_subnet" "name2" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
-    tags = {
-      Name = "my_subnet"
-    }
-  
+  tags = {
+    Name = "my_vpcc"
+  }
 }
+
+# ---------------- Subnet ----------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+# ---------------- Internet Gateway ----------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# ---------------- Route Table ----------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+# ---------------- Route Table Association ----------------
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ---------------- Security Group ----------------
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc
