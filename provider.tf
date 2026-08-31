1afce891fa35b5815ebba5ccb42559e91ee30e84
sasi preetham chandaka <87517348+sasipreethamchandaka@users.noreply.github.com>
2026-08-31 10:30:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,21 @@
-provider "aws" {
-  
-}
-
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">5.0" #morthan 4.0 version
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
